03f366bcd2e9c4e2d7584a61808fc3ff2cafcebe
Nathan Lefler <n.lefler@gmail.com>
2016-02-06 08:33:35 -0500

    Remove parcelables

--- Glucloser/app/src/main/kotlin/com/nlefler/glucloser/dataSource/PlaceFactory.kt
+++ Glucloser/app/src/main/kotlin/com/nlefler/glucloser/dataSource/PlaceFactory.kt
@@ -9,7 +9,6 @@ import com.google.gson.Gson
 import com.nlefler.glucloser.dataSource.jsonAdapter.PlaceJsonAdapter
 import com.nlefler.glucloser.models.CheckInPushedData
 import com.nlefler.glucloser.models.Place
-import com.nlefler.glucloser.models.parcelable.PlaceParcelable
 import com.nlefler.nlfoursquare.Model.Venue.NLFoursquareVenue
 
 import com.squareup.moshi.JsonAdapter
@@ -25,23 +24,30 @@ import javax.inject.Inject
 public class PlaceFactory @Inject constructor(val realmManager: RealmManager) {
     private val LOG_TAG = "PlaceFactory"
 
-    public fun placeForId(id: String): Task<Place?> {
-        return placeForFoursquareId(id, false)
-    }
+    public fun placeForId(id: String, create: Boolean): Task<Place?> {
+        return realmManager.executeTransaction(object: RealmManager.Tx<Place?> {
+            override fun dependsOn(): List<RealmObject?> {
+                return emptyList()
+            }
 
-    public fun parcelableFromFoursquareVenue(venue: NLFoursquareVenue?): PlaceParcelable? {
-        if (venue == null || !IsVenueValid(venue)) {
-            Log.e(LOG_TAG, "Unable to create Place from 4sq venue")
-            return null
-        }
+            override fun execute(dependsOn: List<RealmObject?>, realm: Realm): Place? {
+                if (create && id.isEmpty()) {
+                    val place = realm.createObject<Place>(Place::class.java)
+                    return place
+                }
 
-        val parcelable = PlaceParcelable()
-        parcelable.name = venue.name
-        parcelable.foursquareId = venue.id
-        parcelable.latitude = venue.location.lat
-        parcelable.longitude = venue.location.lng
+                val query = realm.where<Place>(Place::class.java)
 
-        return parcelable
+                query?.equalTo(Place.FoursquareIdFieldName, id)
+                var result: Place? = query?.findFirst()
+
+                if (result == null && create) {
+                    result = realm.createObject<Place>(Place::class.java)
+                }
+                return result
+            }
+
+        })
     }
 
     public fun jsonAdapter(): JsonAdapter<Place> {
@@ -78,41 +84,6 @@ public class PlaceFactory @Inject constructor(val realmManager: RealmManager) {
         })
     }
 
-    public fun parcelableFromPlace(place: Place): PlaceParcelable? {
-        val parcelable = PlaceParcelable()
-        parcelable.name = place.name
-        parcelable.foursquareId = place.foursquareId
-        parcelable.latitude = place.latitude
-        parcelable.longitude = place.longitude
-
-        return parcelable
-    }
-
-    public fun placeFromParcelable(parcelable: PlaceParcelable): Task<Place?> {
-        return placeForFoursquareId(parcelable.foursquareId, true)
-                .continueWithTask(Continuation<Place?, Task<Place?>> placeForId@ { task ->
-            if (task.isFaulted) {
-                return@placeForId task
-            }
-
-            val place = task.result
-            return@placeForId realmManager.executeTransaction(object: RealmManager.Tx<Place?> {
-                override fun dependsOn(): List<RealmObject?> {
-                    return listOf(place)
-                }
-
-                override fun execute(dependsOn: List<RealmObject?>, realm: Realm): Place? {
-                    val livePlace = dependsOn.first() as Place?
-                    livePlace?.name = parcelable.name
-                    livePlace?.foursquareId = parcelable.foursquareId
-                    livePlace?.latitude = parcelable.latitude
-                    livePlace?.longitude = parcelable.longitude
-                    return livePlace
-                }
-            })
-        })
-    }
-
     public fun arePlacesEqual(place1: Place?, place2: Place?): Boolean {
         if (place1 == null || place2 == null) {
             return false
@@ -126,36 +97,44 @@ public class PlaceFactory @Inject constructor(val realmManager: RealmManager) {
         return idOK && nameOK && latOK && lonOK
     }
 
-    public fun placeParcelableFromCheckInData(data: Bundle?): PlaceParcelable? {
+    public fun placeFromCheckInData(data: Bundle?): Task<Place?> {
         if (data == null) {
-            Log.e(LOG_TAG, "Cannot create Place from check-in data, bundle null")
-            return null
+            val msg = "Cannot create Place from check-in data, bundle null"
+            Log.e(LOG_TAG, msg)
+            return Task.forError(Exception(msg))
         }
         val checkInDataSerialized = data.getString("com.parse.Data")
         if (checkInDataSerialized == null || checkInDataSerialized.isEmpty()) {
-            Log.e(LOG_TAG, "Cannot create Place from check-in data, parse bundle null")
-            return null
+            val msg = "Cannot create Place from check-in data, parse bundle null"
+            Log.e(LOG_TAG, msg)
+            return Task.forError(Exception(msg))
         }
         val checkInData = (Gson()).fromJson<CheckInPushedData>(checkInDataSerialized, CheckInPushedData::class.java)
         if (checkInData == null) {
-            Log.e(LOG_TAG, "Cannot create Place from check-in data, couldn't parse data")
-            return null
+            val msg = "Cannot create Place from check-in data, couldn't parse data"
+            Log.e(LOG_TAG, msg)
+            return Task.forError(Exception(msg))
         }
 
-        val placeParcelable = PlaceParcelable()
-        placeParcelable.foursquareId = checkInData.venueId
-        placeParcelable.name = checkInData.venueName
-        if (checkInData.venueLat != 0f) {
-            placeParcelable.latitude = checkInData.venueLat
-        }
-        if (checkInData.venueLon != 0f) {
-            placeParcelable.longitude = checkInData.venueLon
-        }
+        return placeForFoursquareId(checkInData.venueId, true).continueWithTask { task ->
+            if (task.isFaulted) {
+                return@continueWithTask Task.forError<Place?>(task.error)
+            }
 
-        return placeParcelable
+            val place = task.result
+            place?.foursquareId = checkInData.venueId
+            place?.name = checkInData.venueName
+            if (checkInData.venueLat != 0f) {
+                place?.latitude = checkInData.venueLat
+            }
+            if (checkInData.venueLon != 0f) {
+                place?.longitude = checkInData.venueLon
+            }
+            return@continueWithTask  Task.forResult(place)
+        }
     }
 
-    private fun placeForFoursquareId(id: String, create: Boolean): Task<Place?> {
+    public fun placeForFoursquareId(id: String, create: Boolean): Task<Place?> {
         return realmManager.executeTransaction(object: RealmManager.Tx<Place?> {
             override fun dependsOn(): List<RealmObject?> {
                 return emptyList()
