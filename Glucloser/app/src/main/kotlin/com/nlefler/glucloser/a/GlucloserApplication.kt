9e2d2fea115c8c208a796adca83c8599efc57170
Nathan Lefler <n.lefler@gmail.com>
2016-08-04 20:00:45 -0400

    Making a wear app that displays latest CGM data

--- Glucloser/app/src/main/kotlin/com/nlefler/glucloser/a/GlucloserApplication.kt
+++ Glucloser/app/src/main/kotlin/com/nlefler/glucloser/a/GlucloserApplication.kt
@@ -3,8 +3,15 @@ package com.nlefler.glucloser.a
 import android.app.Application
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import android.os.Debug
 import android.support.multidex.MultiDex
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.wearable.DataApi
+import com.google.android.gms.wearable.DataEventBuffer
+import com.google.android.gms.wearable.PutDataMapRequest
+import com.google.android.gms.wearable.Wearable
 import com.nlefler.glucloser.a.R
 import com.nlefler.glucloser.a.components.DaggerRootComponent
 import com.nlefler.glucloser.a.dataSource.sync.cairo.CairoServices
@@ -16,13 +23,25 @@ import com.nlefler.glucloser.a.push.PushRegistrationIntentService
 import com.nlefler.glucloser.a.user.UserManager
 import dagger.Module
 import dagger.Provides
+import rx.Observable
+import rx.Subscription
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 /**
  * Created by Nathan Lefler on 12/12/14.
  */
 @Module
-class GlucloserApplication : Application() {
+class GlucloserApplication : Application(),
+        DataApi.DataListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private val SENSOR_READING_KEY = "com.nlefler.glucloser.a.datakey.sensor_reading"
+
+    private var googleApiClient: GoogleApiClient? = null
+    private var sensorSub: Subscription? = null
+
     val rootComponent = DaggerRootComponent.builder().glucloserApplication(this).build()
 
     override fun attachBaseContext(context: Context) {
@@ -43,10 +62,32 @@ class GlucloserApplication : Application() {
 
         this.subscribeToPush()
 
+
     }
 
     private fun subscribeToPush() {
         startService(Intent(this, PushRegistrationIntentService::class.java))
+    }
+
+    override fun onDataChanged(p0: DataEventBuffer?) {
+
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        sensorSub?.unsubscribe()
+        sensorSub = Observable.interval(10, TimeUnit.MINUTES).doOnNext {
+            val putDataMapReq = PutDataMapRequest.create("/sensor_reading")
+            putDataMapReq.dataMap.putInt(SENSOR_READING_KEY, 999)
+            val putDataReq = putDataMapReq.asPutDataRequest()
+            val pendingResult = Wearable.DataApi.putDataItem(googleApiClient, putDataReq)
+        }.subscribe()
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        sensorSub?.unsubscribe()
     }
 
     // ContextComponent
---
+++ Glucloser/wearCGM/src/main/java/com/nlefler/glucloser/a/wear/MainActivity.java
@@ -0,0 +1,140 @@
+package com.nlefler.glucloser.a.wear;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.view.BoxInsetLayout;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends WearableActivity implements
+        DataApi.DataListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String SENSOR_READING_KEY = "com.nlefler.glucloser.a.wear.datakey.sensor_reading";
+
+    private GoogleApiClient mGoogleApiClient;
+    private int sensorReading = 0;
+
+    private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
+            new SimpleDateFormat("HH:mm", Locale.US);
+
+    private BoxInsetLayout mContainerView;
+    private TextView mTextView;
+    private TextView mClockView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        setAmbientEnabled();
+
+        mContainerView = (BoxInsetLayout) findViewById(R.id.container);
+        mTextView = (TextView) findViewById(R.id.text);
+        mClockView = (TextView) findViewById(R.id.clock);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Wearable.DataApi.addListener(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Wearable.DataApi.removeListener(mGoogleApiClient, this);
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        for (DataEvent event : dataEvents) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                // DataItem changed
+                DataItem item = event.getDataItem();
+                if (item.getUri().getPath().compareTo("/count") == 0) {
+                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                    updateSensorReading(dataMap.getInt(SENSOR_READING_KEY));
+                }
+            } else if (event.getType() == DataEvent.TYPE_DELETED) {
+                // DataItem deleted
+            }
+        }
+    }
+
+
+    @Override
+    public void onEnterAmbient(Bundle ambientDetails) {
+        super.onEnterAmbient(ambientDetails);
+        updateDisplay();
+    }
+
+    @Override
+    public void onUpdateAmbient() {
+        super.onUpdateAmbient();
+        updateDisplay();
+    }
+
+    @Override
+    public void onExitAmbient() {
+        updateDisplay();
+        super.onExitAmbient();
+    }
+
+    private void updateDisplay() {
+
+        if (isAmbient()) {
+            mContainerView.setBackgroundColor(getResources().getColor(android.R.color.black));
+            mTextView.setTextColor(getResources().getColor(android.R.color.white));
+            mClockView.setVisibility(View.VISIBLE);
+
+            mClockView.setText(AMBIENT_DATE_FORMAT.format(new Date()));
+        } else {
+            mContainerView.setBackground(null);
+            mTextView.setTextColor(getResources().getColor(android.R.color.black));
+            mClockView.setVisibility(View.GONE);
+        }
+    }
+
+    private void updateSensorReading(int reading) {
+        mTextView.setText(String.valueOf(reading));
+    }
+}
